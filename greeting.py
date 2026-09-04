2f898b83bf85c985f99912f1dd4b1af80b4bd99f
RaneemZi <raneemmahmoud333@gmail.com>
2026-09-04 13:56:27 +0300

    solve merge conflict in file greeting

--- greeting.py
+++ greeting.py
@@ -1,6 +1,6 @@
 def greeting(name):
-    return f"Hi, {name}! Feature branch says hello."
-    return f"Hello, {name}! Welcome to Git week."
+    return f"Hi, {name}! Feature branch says hello. Hello, {name}! Welcome to Git week."
+    
 
 if __name__ == "__main__":
     print(greeting("Student"))
